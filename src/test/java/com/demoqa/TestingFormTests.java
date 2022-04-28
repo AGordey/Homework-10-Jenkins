9d78d03b9eb08aa3d62e5edfb7a14db4e7290d2e
AGordey <gordey.andrej@gmail.com>
2022-04-28 21:56:22 +0300

    Jenkins+Test

--- src/test/java/com/demoqa/TestingFormTests.java
+++ src/test/java/com/demoqa/TestingFormTests.java
@@ -12,7 +12,6 @@ import static io.qameta.allure.Allure.step;
 
 public class TestingFormTests {
 
-
     @Test
     void fillFormTest() {
         step("Open form on site", () -> {
@@ -21,53 +20,53 @@ public class TestingFormTests {
             executeJavaScript("$('#fixedban').remove()");
         });
         step("Fill form ", () -> {
-        $("#firstName").setValue("OurFirstName");
-        $("#lastName").setValue("OurLastName");
-        $("#userEmail").setValue("OurEmail@email.com");
-        $("#userNumber").setValue("1234567890");
-        $("#currentAddress").setValue("OurCurrentAddress");
+            $("#firstName").setValue("OurFirstName");
+            $("#lastName").setValue("OurLastName");
+            $("#userEmail").setValue("OurEmail@email.com");
+            $("#userNumber").setValue("1234567890");
+            $("#currentAddress").setValue("OurCurrentAddress");
 
-        //Поле с радиобаттоном - выбор пола
-        $("#genterWrapper").$(byText("Male")).click();
+            //Поле с радиобаттоном - выбор пола
+            $("#genterWrapper").$(byText("Male")).click();
 
-        //Дата рождения
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption("2000");
-        $(".react-datepicker__month-select").selectOption("January");
-        $(".react-datepicker__day.react-datepicker__day--019").click();
+            //Дата рождения
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__year-select").selectOption("2000");
+            $(".react-datepicker__month-select").selectOption("January");
+            $(".react-datepicker__day.react-datepicker__day--019").click();
 
-        //Поле предметов - выпадающее меню
-        $("#subjectsInput").setValue("Hindi").pressEnter();
+            //Поле предметов - выпадающее меню
+            $("#subjectsInput").setValue("Hindi").pressEnter();
 
-        //Выбор хобби
-        $("#hobbiesWrapper").$(byText("Reading")).click();
+            //Выбор хобби
+            $("#hobbiesWrapper").$(byText("Reading")).click();
 
-        //Выбор города
-        $("#stateCity-wrapper").click();
-        $(byText("Haryana")).click();
-        $("#city").click();
-        $(byText("Karnal")).click();
+            //Выбор города
+            $("#stateCity-wrapper").click();
+            $(byText("Haryana")).click();
+            $("#city").click();
+            $(byText("Karnal")).click();
 
-        //Зaгрузка файла
-        $("#uploadPicture").uploadFromClasspath("1.png");
+            //Зaгрузка файла
+            $("#uploadPicture").uploadFromClasspath("1.png");
 
-        //Кнопка Submit
-        $("#submit").click();
+            //Кнопка Submit
+            $("#submit").click();
         });
         step("Verify form ", () -> {
-        //Проверка введенных данных
-        $(".modal-body").shouldHave
-                (text("OurFirstName"),
-                        text("OurLastName"),
-                        text("OurEmail@email.com"),
-                        text("1234567890"),
-                        text("Male"),
-                        text("Hindi"),
-                        text("Reading"), text("Haryana"),
-                        text("Karnal"),
-                        text("1.png"),
-                        text("19 January,2000"),
-                        text("OurCurrentAddress"));
-    });
+            //Проверка введенных данных
+            $(".modal-body").shouldHave
+                    (text("OurFirstName"),
+                            text("OurLastName"),
+                            text("OurEmail@email.com"),
+                            text("1234567890"),
+                            text("Male"),
+                            text("Hindi"),
+                            text("Reading"), text("Haryana"),
+                            text("Karnal"),
+                            text("1.png"),
+                            text("19 January,2000"),
+                            text("OurCurrentAddress"));
+        });
     }
 }
